bbd46e9bbbf23b2bbc936c166aee2a5c3e3badbe
Fidel Pizarro <fidelpizarro11@gmail.com>
2026-04-12 17:54:19 -0300

    agregue el home y estaddo global de peliculas, conecte con localstorage usando useeffect y pase a medialist por props las peliculas

--- src/Components/MediaList/MediaList.jsx
+++ src/Components/MediaList/MediaList.jsx
@@ -1,36 +1,22 @@
 
-import { useState,useEffect } from "react";
+function MediaList({peliculas}) {
 
-function MediaList() {
-
-    const [porVer, setPorVer] = useState([])
-    const [visto, setVisto] = useState([])
-
-    //1-LEER. este useEffect lee el contenido de localstorage al renderizarse la pagina solo una vez, lee lo que habia y lo mete en el estado
-    useEffect(()=> {
-        const porVerGuardado = JSON.parse(localStorage.getItem("porVer")) || []
-        setPorVer(porVerGuardado)
-        const vistoGuardado = JSON.parse(localStorage.getItem("visto")) || []
-        setVisto(vistoGuardado)
-    },[])
-        //2-GUARDAR. lo que hace este useEffect es ejecutarse cada vez que por ver o visto cambien, asi si se quita o agrega una pelicula se guarde automaticamente
-    useEffect(() => {
-        localStorage.setItem("porVer", JSON.stringify(porVer))
-        localStorage.setItem("visto", JSON.stringify(visto))
-        }, [porVer, visto])
-
+        const listaPorVer = peliculas.filter(item =>item.visto === false)
+        const listaVista = peliculas.filter(item =>item.visto === true)
 
     return (
         <div>
             <div>
                 <h2>Por Ver</h2>
-                {porVer.length === 0 ? <p>Vacio, ya viste todo</p> : porVer.map((item) =>(
-                <p key={item.titulo}>{item.titulo}</p>)) }
+                {listaPorVer.length === 0 ? <p>Vacio, ya viste todo</p> : listaPorVer.map(item =>(
+                    <p key={item.titulo}>{item.titulo} </p>
+                )) }
             </div>
             <div>
                 <h2>Visto</h2>
-                {visto.length === 0? <p>Aun no viste nada</p>: visto.map((item) =>(
-                    <p key={item.titulo}>{item.titulo}</p>))}
+                {listaVista.length === 0? <p>Aun no viste nada</p>: listaVista.map(item =>(
+                    <p key={item.titulo}>{item.titulo} </p>
+                )) }
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import MediaList from "../components/MediaList/MediaList"
 
 function Home () {
-
     const [peliculas,setPeliculas] =useState([])
-    
+    const [busqueda,setBusqueda] = useState("")
+    const [filtros,setFiltros] = useState({genero:"",tipo:""})
+    const [orden,setOrden] = useState({ campo: "titulo", direccion:"asc"})
 
+    //este useeffect ve si hay peliculas guardadas en localstorage, y las guarda con el setpeliculas
+    useEffect(()=> {
+        const peliculasGuardadas = JSON.parse(localStorage.getItem("peliculas")) || []
+        setPeliculas(peliculasGuardadas)
+}, [])
 
-
+    //este useeffect guarda en localstorage la lista de peliculas cada vez que ve una modificacion en peliculas
+    useEffect(() =>{
+        localStorage.setItem("peliculas",JSON.stringify(peliculas))   
+        },[peliculas])
     return(
-        <MediaList/>
-    )
+    <MediaList peliculas={peliculas}/>
+
+    )   
 }
 
 export default Home
