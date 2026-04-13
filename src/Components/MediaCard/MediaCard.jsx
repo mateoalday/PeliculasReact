32446faa827383503b2bb8eb70bef5e41fe3b0e5
joaquin vulcano <vulcanojoaquin@gmail.com>
2026-04-13 20:21:52 -0300

    estilo implementado

--- src/Components/MediaCard/MediaCard.jsx
+++ src/Components/MediaCard/MediaCard.jsx
@@ -1,7 +1,9 @@
+import styles from './MediaCard.module.css';
+
 const MediaCard = ({ item, accionEliminar, accionEditar, accionCambiarEstado, textoBotonEstado }) => {
   return (
-    <div>
-      <h3>{item.titulo}</h3>
+    <div className={styles.card}>
+      <h3 className={styles.title}>{item.titulo}</h3>
       <p><strong>Director:</strong> {item.director}</p>
       <p><strong>Año:</strong> {item.año}</p>
       <p><strong>Género:</strong> {item.genero}</p>
